6418d130ab28dbc0a27f08f9c22e25b3a8b8f955
LJared5 <jared.ochoa005@gmail.com>
2025-05-08 20:43:30 -0500

    Se agrega el proyecto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang= "js" setup>
+  import DataTable from './components/DataTable.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Tabla De Datos</h1>
+      <DataTable />   
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: red;
+  }
+</style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,76 @@
+<script setup>
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+import { ref } from 'vue'
+const nombre = ref('')
+const apellido = ref('')
+const edad = ref('')
+const genero = ref('')
+const cedula = ref('')
+const personas = ref([])
+let idContador = 1;
+
+function guardarDatos() {
+    const nuevaPersona = {
+        id: idContador++,
+        nombre: nombre.value,
+        apellido: apellido.value,
+        edad: edad.value,
+        genero: genero.value,
+        cedula: cedula.value
+    }
+    personas.value.push(nuevaPersona)
+}
+
+
+</script>
+<template>
+    <div class="form-container">
+        <div class="card flex justify-center">
+            <InputText name= "Nombre" type="text" v-model="nombre" placeholder="Nombre" />
+        </div>
+        <div class="card flex justify-center">
+            <InputText name= "Apellido" type="text" v-model="apellido" placeholder="Apellido" />
+        </div>
+        <div class="card flex justify-center">
+            <InputText name= "Edad" type="number" v-model="edad" placeholder="Edad" />
+        </div>
+        <div class="card flex justify-center">
+            <InputText name= "Genero" type="text" v-model="genero" placeholder="Genero" />
+        </div>
+        <div class="card flex justify-center">
+            <InputText name= "Cedula" type="text" v-model="cedula" placeholder="Cedula" />
+        </div>
+        <div class="card flex justify-center ">
+            <Button label="Guardar" @click="guardarDatos"/>
+        </div>      
+    </div>
+    <div class="card">
+        <DataTable :value="personas" :showGridlines="true" tableStyle="min-width: 50rem ">
+            <Column field="id" header="ID"></Column>
+            <Column field="nombre" header="Nombre"></Column>
+            <Column field="apellido" header="Apellido"></Column>
+            <Column field="edad" header="Edad"></Column>
+            <Column field="genero" header="Genero"></Column>
+            <Column field="cedula" header="Cedula"></Column>
+        </DataTable>
+    </div>
+</template>
+  
+  
+<style scoped>
+    .form-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+    }
+    div.card {
+        margin: 10px;
+    }
+
+</style>
+  
